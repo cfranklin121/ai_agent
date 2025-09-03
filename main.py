bc5d24723c62fadf6642d20b624d445ccf05b3af
cfranklin121 <courtenayfranklin@gmail.com>
2025-09-02 19:35:28 -0700

    LLMs, Input

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello from ai-agent!")
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
 
+def main():
+    if len(sys.argv) < 2:
+        print("No prompt given. Please provide a prompt.")
+        sys.exit(1)
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=[sys.argv[1]]
+    )
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print("Response:")
+    print(response.text)
 
 if __name__ == "__main__":
     main()
