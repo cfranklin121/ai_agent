41d73fdd9951207494b064098098e992527967a5
cfranklin121 <courtenayfranklin@gmail.com>
2025-09-03 19:38:17 -0700

    Complete LLMs

--- main.py
+++ main.py
@@ -2,22 +2,41 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
-    if len(sys.argv) < 2:
+    load_dotenv()
+
+    args = sys.argv[1:]
+    verbose_flag = False
+    if not args:
         print("No prompt given. Please provide a prompt.")
         sys.exit(1)
-    load_dotenv()
+   
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    if args[-1] == "--verbose":
+        verbose_flag = True
+        args = args[:-1]
+    user_prompt = " ".join(args)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=[sys.argv[1]]
+        contents=messages,
     )
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print("Response:")
-    print(response.text)
+
+    if verbose_flag:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print(response.text)
+
+    else:
+        print(response.text)
 
 if __name__ == "__main__":
     main()
