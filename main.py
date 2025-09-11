7e4289e543722198f7ca8a2dfd387b66515841fe
cfranklin121 <courtenayfranklin@gmail.com>
2025-09-10 20:56:09 -0700

    Function Calling: Function Declaration

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-
 from prompts import system_prompt
+
+from call_function import available_functions
 
 def main():
     load_dotenv()
 
-    
     args = sys.argv[1:]
     verbose_flag = False
     if not args:
@@ -18,6 +18,8 @@ def main():
    
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+
+
     if args[-1] == "--verbose":
         verbose_flag = True
         args = args[:-1]
@@ -30,15 +32,20 @@ def main():
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt),
     )
 
+    if response.function_calls:
+        function_call_part = response.function_calls[0] 
     if verbose_flag:
         print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-        print(response.text)
-
+       
+    if response.function_calls:
+        for function_call_part in response.function_calls:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
     else:
         print(response.text)
 
